41cd5b5c493f2dec27725a5c5f5a41f9a43fee7b
editionc18 <editionc18@gmail.com>
2022-01-22 17:37:19 +0900

    테스트용 로그아웃 버튼 추가

--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:meeco_app/backend/api_provider.dart';
+import 'package:provider/provider.dart';
 
 class UserPage extends StatefulWidget {
   const UserPage({Key? key}) : super(key: key);
@@ -10,6 +14,39 @@ class UserPage extends StatefulWidget {
 class _UserPageState extends State<UserPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final apiProvider = Provider.of<ApiProvider>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: ClipRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+            child: Container(color: Colors.transparent),
+          ),
+        ),
+        iconTheme: const IconThemeData(color: Colors.black),
+        backgroundColor: const Color(0x78bfbfbf),
+        // const Color(0xff4c5c84),
+        elevation: 0,
+        centerTitle: true,
+      ),
+      body: Center(
+        child: TextButton(
+          child: const Text(
+            '로그아웃',
+            style: TextStyle(
+              color: Colors.red,
+              fontSize: 20,
+            ),
+          ),
+          onPressed: () async {
+            await apiProvider.logOut();
+            if (!apiProvider.isLoggedIn) {
+              Navigator.pop(context);
+            }
+          },
+        ),
+      ),
+    );
   }
 }
