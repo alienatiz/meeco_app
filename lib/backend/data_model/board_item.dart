b9de597cacb5a1d3bc31e1b0e0179cf8c013c491
editionc18 <editionc18@gmail.com>
2022-01-25 09:20:39 +0900

    BoardItem.toString 제거

--- lib/backend/data_model/board_item.dart
+++ lib/backend/data_model/board_item.dart
@@ -15,20 +15,14 @@ class BoardItem {
   int commentNum;
 
   BoardItem(
-      this.url,
-      this.category,
-      this.title,
-      this.author,
-      this.time,
-      this.viewNum,
-      this.voteNum,
-      this.commentNum, {
-        this.isNotice = false,
-      });
-
-  @override
-  String toString() {
-    return (isNotice ? "[공지]" : "") +
-        "<$url>$author: $title [$viewNum 조회][$voteNum 공감][$commentNum개의 댓글]";
-  }
+    this.url,
+    this.category,
+    this.title,
+    this.author,
+    this.time,
+    this.viewNum,
+    this.voteNum,
+    this.commentNum, {
+    this.isNotice = false,
+  });
 }
