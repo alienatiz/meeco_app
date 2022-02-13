65afb6022b5184f184ac7ac86ddf76f9c3d84d70
editionc18 <editionc18@gmail.com>
2022-02-13 19:38:31 +0900

    BottomNavigationBar로 구현, fluttertoast 패키지 추가

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:meeco_app/backend/board_provider.dart';
 import 'package:meeco_app/backend/tab_provider.dart';
-import 'package:meeco_app/pages/user_page.dart';
+import 'package:meeco_app/constants.dart';
+import 'package:meeco_app/pages/profile_page.dart';
 import 'package:meeco_app/widgets/board_item_view.dart';
 import 'package:meeco_app/widgets/custom_circular_progress_indicator.dart';
 import 'package:provider/provider.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -15,6 +17,15 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  late FToast fToast;
+
+  @override
+  void initState() {
+    super.initState();
+    fToast = FToast();
+    fToast.init(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     final Map<String, String>? arg =
@@ -23,23 +34,7 @@ class _MainPageState extends State<MainPage> {
             : {'title': 'IT+', 'url': 'ITplus'};
     final TabProvider tabProvider = Provider.of<TabProvider>(context);
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.menu),
-          onPressed: () {
-            Navigator.pushReplacementNamed(context, '/menu');
-          },
-        ),
-        title: Text(arg!['title']!),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.account_circle_outlined),
-            onPressed: () {
-              Navigator.pushNamed(context, '/user');
-            },
-          ),
-        ],
-      ),
+      appBar: _buildAppBar(arg!['title']!),
       body: _buildBody(),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: tabProvider.currentIndex,
@@ -54,7 +49,7 @@ class _MainPageState extends State<MainPage> {
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.account_circle_outlined),
-            label: '설정',
+            label: '프로필',
           )
         ],
         onTap: (index) {
@@ -64,19 +59,116 @@ class _MainPageState extends State<MainPage> {
     );
   }
 
+  _buildAppBar(String title) {
+    switch (Provider.of<TabProvider>(context).currentIndex) {
+      case 0:
+        return ListPageAppBar(title: title, fToast: fToast);
+      case 1:
+        return NotiPageAppBar();
+      case 2:
+        return ProfilePageAppBar();
+    }
+  }
+
   _buildBody() {
     switch (Provider.of<TabProvider>(context).currentIndex) {
       case 0:
         return const ListPage();
       case 1:
-        return const Center(
-          child: Text('알림이 없습니다.'),
+        return Center(
+          child: Text(
+            '알림 기능은 준비중에 있습니다.',
+            style: Theme.of(context).textTheme.bodyText1,
+          ),
         );
       case 2:
         return const UserPage();
       default:
         return Container();
     }
+  }
+}
+
+class ListPageAppBar extends StatelessWidget with PreferredSizeWidget {
+  @override
+  final Size preferredSize;
+  ListPageAppBar({
+    Key? key,
+    required this.title,
+    required this.fToast,
+  })  : preferredSize = const Size.fromHeight(52.0),
+        super(key: key);
+
+  final String title;
+  final FToast fToast;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+        icon: const Icon(Icons.menu),
+        onPressed: () {
+          Navigator.pushReplacementNamed(context, '/menu');
+        },
+      ),
+      title: Text(title),
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.edit_outlined),
+          iconSize: 21,
+          onPressed: () {
+            // Navigator.pushNamed(context, '/write');
+            fToast.showToast(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: secondaryColor,
+                  borderRadius: BorderRadius.circular(40.0),
+                ),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20.0,
+                  vertical: 10.0,
+                ),
+                child: const Text(
+                  '글쓰기 기능은 준비 중에 있습니다.',
+                  style: TextStyle(
+                    color: textDark,
+                  ),
+                ),
+              ),
+              gravity: ToastGravity.BOTTOM,
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class NotiPageAppBar extends StatelessWidget with PreferredSizeWidget {
+  @override
+  final Size preferredSize;
+
+  NotiPageAppBar({Key? key})
+      : preferredSize = const Size.fromHeight(52.0),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(title: const Text('알림'));
+  }
+}
+
+class ProfilePageAppBar extends StatelessWidget with PreferredSizeWidget {
+  @override
+  final Size preferredSize;
+
+  ProfilePageAppBar({Key? key})
+      : preferredSize = const Size.fromHeight(52.0),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(title: const Text('프로필'));
   }
 }
 
@@ -103,6 +195,7 @@ class _ListPageState extends State<ListPage> {
         controller: refreshController,
         onRefresh: _onRefresh,
         child: ListView.separated(
+          padding: const EdgeInsets.only(top: 8.0),
           physics: const BouncingScrollPhysics(),
           itemCount: items.length + 1,
           itemBuilder: (_, index) {
