1b7237f12eff2cd2c86d6e1584a47b25e0c8d24c
editionc18 <editionc18@gmail.com>
2022-02-23 21:40:08 +0900

    BoardItem 파싱 부분을 BoardItem.fromElement 함수로 이동

--- lib/backend/board_provider.dart
+++ lib/backend/board_provider.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:html/parser.dart';
-import 'package:html/dom.dart' as dom;
 import 'package:meeco_app/backend/client.dart';
 import 'package:meeco_app/backend/data_model/board_item.dart';
 
@@ -14,7 +13,7 @@ class BoardProvider extends ChangeNotifier {
 
   BoardProvider({required this.client});
 
-  fetchItems() async {
+  Future<void> fetchItems() async {
     loading = true;
     notifyListeners();
 
@@ -26,7 +25,7 @@ class BoardProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  switchBoard(String board) async {
+  Future<void> switchBoard(String board) async {
     if (currentBoard != board) {
       loading = true;
       notifyListeners();
@@ -38,7 +37,7 @@ class BoardProvider extends ChangeNotifier {
     }
   }
 
-  refresh() async {
+  Future<void> refresh() async {
     loading = true;
     notifyListeners();
 
@@ -53,59 +52,13 @@ class BoardProvider extends ChangeNotifier {
 
     var docListBody = parse(docList.body)
         .querySelectorAll('table.ldn > tbody > tr')
-        .map(_parseBoard)
+        .map(BoardItem.fromElement)
         .toList();
 
     if (page > 1) {
-      docListBody = docListBody.where((element) => !element.isNotice).toList();
+      return docListBody.where((element) => !element.isNotice).toList();
     }
 
     return docListBody;
-  }
-
-  BoardItem _parseBoard(dom.Element el) {
-    final String? categoryText = _getInnerText(el, 'td.title > a.boardname');
-    final String? url = _replaceUrl(
-      el.querySelector('td.title > a.title_a')?.attributes['href'] ?? '/',
-    );
-    final String? title = _getInnerText(el, 'td.title > a.title_a > span');
-    final int commentNum = int.parse(
-        _getInnerText(el, 'td.title > a[title="Replies"]')
-                ?.replaceAll(RegExp('[\\[\\]]'), '') ??
-            '0');
-    // final memberSrl = element.querySelector('td.author > a')?.className ?? '0';
-    final String? author = _getInnerText(el, 'td.author > a');
-    final numData =
-        el.querySelectorAll('td.num').map((e) => e.text.trim()).toList();
-    final String time = numData[1];
-    final int voteNum = int.tryParse(numData[2]) ?? 0;
-    final int viewNum = int.tryParse(numData[3]) ?? 0;
-    final isNotice = numData[0] == "공지";
-
-    return BoardItem(
-      url: url ?? '',
-      categoryText: categoryText ?? '--',
-      title: title ?? '제목',
-      author: author ?? '작성자',
-      time: time,
-      viewNum: viewNum,
-      voteNum: voteNum,
-      commentNum: commentNum,
-      isNotice: isNotice,
-    );
-  }
-
-  String _replaceUrl(String url) {
-    if (double.tryParse(url.split('/').last) == null) {
-      final urlMatches = RegExp('mid=([A-Za-z]+)/document_srl=([0-9]+)')
-          .allMatches(url)
-          .elementAt(0);
-      return '/${urlMatches.group(1)}/${urlMatches.group(2)}';
-    }
-    return url;
-  }
-
-  String? _getInnerText(dom.Element element, String query) {
-    return element.querySelector(query)?.text.trim();
   }
 }
--- lib/backend/data_model/board_item.dart
+++ lib/backend/data_model/board_item.dart
@@ -1,3 +1,5 @@
+import 'package:html/dom.dart' as dom;
+
 class BoardItem {
   String url;
 
@@ -23,4 +25,72 @@ class BoardItem {
     required this.commentNum,
     this.isNotice = false,
   });
+
+  factory BoardItem.fromElement(dom.Element element) {
+    final tdList = element.querySelectorAll('td');
+
+    final bool isNotice = _getIsNotice(tdList[0]);
+
+    final Map<String, dynamic> titleData = _parseTitle(tdList[1]);
+    final String categoryText = titleData['categoryText'];
+    final String url = titleData['url'];
+    final String title = titleData['title'];
+    final int commentNum = titleData['commentNum'];
+
+    final String author = _getAuthor(tdList[2]);
+    final String time = _getTime(tdList[3]);
+    final int voteNum = _getNum(tdList[4]);
+    final int viewNum = _getNum(tdList[5]);
+
+    return BoardItem(
+      url: url,
+      isNotice: isNotice,
+      categoryText: categoryText,
+      title: title,
+      author: author,
+      time: time,
+      viewNum: viewNum,
+      voteNum: voteNum,
+      commentNum: commentNum,
+    );
+  }
+
+  static bool _getIsNotice(dom.Element element) {
+    return element.text == '공지';
+  }
+
+  static Map<String, dynamic> _parseTitle(dom.Element element) {
+    final List<dom.Element> children = element.querySelectorAll('a');
+
+    final String categoryText = children[0].text;
+    final String url = children[1].attributes['href'] ?? '/';
+    final String title = children[1].querySelector('span')?.text ?? '제목';
+    late final int commentNum;
+    if (children.length == 3) {
+      commentNum =
+          int.parse(children[2].text.trim().replaceAll(RegExp('[\\[\\]]'), ''));
+    } else {
+      commentNum = 0;
+    }
+
+    return {
+      'categoryText': categoryText,
+      'title': title,
+      'commentNum': commentNum,
+      'url': url,
+    };
+  }
+
+  static String _getAuthor(dom.Element element) {
+    return element.querySelector('a')?.text ?? '작성자';
+  }
+
+  static String _getTime(dom.Element element) {
+    return element.text;
+  }
+
+  static int _getNum(dom.Element element) {
+    final String numString = element.querySelector('span')?.text ?? '0';
+    return int.parse(numString);
+  }
 }
